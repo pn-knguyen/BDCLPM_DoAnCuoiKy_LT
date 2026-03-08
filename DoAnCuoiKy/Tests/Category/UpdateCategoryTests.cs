5998f2a7805164fd827e5b6b8c0900521aba5279
pn-knguyen <phamnguyen27082005@gmail.com>
2026-03-08 12:20:57 +0700

    update folder Tests

---
+++ DoAnCuoiKy/Tests/Category/UpdateCategoryTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoAnCuoiKy.Tests.Category
+{
+    internal class UpdateCategoryTests
+    {
+    }
+}
---
+++ DoAnCuoiKy/Tests/Product/UpdateProductTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoAnCuoiKy.Tests.Product
+{
+    internal class UpdateProductTests
+    {
+    }
+}
