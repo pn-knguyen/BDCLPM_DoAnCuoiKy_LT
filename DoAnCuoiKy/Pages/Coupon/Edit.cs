4a74d58e8b0c23c67403ce9a7b8aba38984fab62
btrne <nnm218243@gmail.com>
2026-03-30 07:49:09 +0700

    Test Create Coupon

---
+++ DoAnCuoiKy/Pages/Coupon/Edit.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DoAnCuoiKy.Pages.Coupon
+{
+    public class Edit
+    {
+        private readonly IWebDriver _driver;
+        private readonly WebDriverWait _wait;
+
+        public Edit(IWebDriver driver, WebDriverWait wait)
+        {
+            _driver = driver;
+            _wait = wait;
+        }
+
+        public void UpdateExpiryDate(string newDate)
+        {
+            var input = _wait.Until(d => d.FindElement(By.Name("ExpiryDate"))); //
+            input.Clear();
+            input.SendKeys(newDate);
+        }
+
+        public void SetUsedStatus(bool used)
+        {
+            var checkbox = _wait.Until(d => d.FindElement(By.Name("IsUsed"))); //
+            if (checkbox.Selected != used) checkbox.Click();
+        }
+
+        public void Submit() => _driver.FindElement(By.XPath("//button[contains(text(), 'Cập nhật')]")).Click(); //
+    }
+}
